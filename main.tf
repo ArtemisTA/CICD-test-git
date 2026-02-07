cc894bb0fb661cd4e1969f1ea98ff80879535379
Artemis-Ash <artemis.ash@city.ac.uk>
2026-02-07 11:44:58 +0000

    Test Cloud Build trigger

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "cicd-test-project-486709"
+  region  = "europe-west2"
+}
+
+resource "google_storage_bucket" "demo" {
+  name                        = "artemis-ci-bucket-20260207-001"
+  location                    = "EU"
+  uniform_bucket_level_access = true
+  force_destroy               = true
+}
+
+#ypaaaaaa
